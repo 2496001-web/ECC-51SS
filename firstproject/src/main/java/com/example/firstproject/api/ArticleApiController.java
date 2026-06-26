5c62527762a66943c9a7f011c2938a0509788aa2
강예은 <2496001@ewha.ac.kr>
2026-06-26 20:27:16 +0900

    ECC 백엔드 15주차 firstproject 수정

--- firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class ArticleApiController {
     @Autowired // 게시글 리파지터리 주입
     private ArticleService articleService; // 서비스 객체 주입
+    @Autowired
     private ArticleRepository articleRepository; // 리파지터리 객체 주입
     // GET
     // 모든 게시글 조회
@@ -31,7 +32,7 @@ public class ArticleApiController {
         return articleService.show(id);
     }
     // POST
-    @PostMapping("/api.articles")
+    @PostMapping("/api/articles")
     public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
         Article created = articleService.create(dto);
         return (created != null) ?
@@ -42,19 +43,37 @@ public class ArticleApiController {
     // PATCH
     @PatchMapping("/api/articles/{id}")
     public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
-        Article updated = articleService.update(id, dto);
-        return (updated != null) ?
-                ResponseEntity.status(HttpStatus.OK).body(updated) :
-                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        // 1. DTO -> 엔티티 변환하기
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
 
+        // 2. 타깃 조회하기
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 3. 잘못된 요청 처리하기
+        if(target == null || !id.equals(article.getId())) {
+            //400, 잘못된 요청 응답!
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        // 4. 업데이트 및 정상 응답(200)하기
+        target.patch(article); // 기존 데이터에 새 데이터 붙이기
+        Article updated = articleRepository.save(target); // 수정 내용 DB에 최종 저장
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
     // DELETE
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Article> delete(@PathVariable Long id) {
-        Article deleted = articleService.delete(id); // 서비스를 통해 게시글 삭제
-        return (deleted != null) ?
-                ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
-                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        // 1. 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+        // 2. 잘못된 요청 처리하기
+        if (target == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        // 3. 대상 삭제하기
+        articleRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
     @PostMapping("/api/transaction-test") // 여러 게시글 생성 요청 접수
     public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos) {
